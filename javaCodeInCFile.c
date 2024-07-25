6aa747ac548c6ddf9aa846d3e6b2cc388e49b726
Ashish-Chavan <110707870+Ashish-Chavan@users.noreply.github.com>
2024-07-25 19:53:49 +0530

    Update javaCodeInCFile.c

--- javaCodeInCFile.c
+++ javaCodeInCFile.c
@@ -6,9 +6,7 @@ class GFG {
     // A utility function to swap two elements
     static void swap(int[] arr, int i, int j)
     {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        
     }
 
     // This function takes last element as pivot,
